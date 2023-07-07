3595367b831f7959e3fd6b7f4d797aec0297c359
Charles Machalow <csm10495@gmail.com>
2023-07-07 15:49:16 -0700

    fixes

--- repo-setup.py
+++ repo-setup.py
@@ -4,15 +4,15 @@ import pathlib
 REPO_DIR = pathlib.Path(__file__).parent.resolve()
 
 QUESTIONS = {
-    "NAME": "Your first/last name",
-    "EMAIL": "Your email address",
-    "PROJECT": "The name of the project",
+    "[NAME]": "Your first/last name",
+    "[EMAIL]": "Your email address",
+    "[PROJECT]": "The name of the project",
 }
 
 
 def replace_in_file_names(qanda: dict[str, str]):
     for path in REPO_DIR.glob("**/*"):
-        if ".git" not in path.parts:
+        if ".git" not in path.parts and path.name != "repo-setup.py":
             if path.is_file() or path.is_dir():
                 for q, a in qanda.items():
                     path.rename(path.with_name(path.name.replace(q, a)))
@@ -20,16 +20,16 @@ def replace_in_file_names(qanda: dict[str, str]):
 
 def replace_in_files(qanda: dict[str, str]):
     for path in REPO_DIR.glob("**/*"):
-        if ".git" not in path.parts:
+        if ".git" not in path.parts and path.name != "repo-setup.py":
             if path.is_file():
-                text = path.read_text()
+                text = path.read_bytes()
                 for q, a in qanda.items():
-                    text = text.replace(q, a)
-                path.write_text(text)
+                    text = text.replace(q.encode(), a.encode())
+                path.write_bytes(text)
 
 
 if __name__ == "__main__":
-    qanda = {"YEAR": str(datetime.datetime.now().year)}
+    qanda = {"[YEAR]": str(datetime.datetime.now().year)}
     for key, question in QUESTIONS.items():
         value = input(question + ": ")
         qanda[key] = value.strip()
